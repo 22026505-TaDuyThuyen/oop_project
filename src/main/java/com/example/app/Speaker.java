ab3c747b33fe3f7bb368c5ef6ecc1c7182406310
Ta Duy Thuyen <22026505@vnu.edu.vn>
2023-12-13 12:39:43 +0700

    speaker

--- src/main/java/com/example/app/Speaker.java
+++ src/main/java/com/example/app/Speaker.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import com.example.cmd.Word;
 import com.voicerss.tts.AudioCodec;
 import com.voicerss.tts.AudioFormat;
 import com.voicerss.tts.Languages;
@@ -16,7 +17,7 @@ import com.voicerss.tts.VoiceProvider;
 import javafx.scene.control.Alert;
 
 // phát âm
-public class Speaker {
+public class Speaker extends Thread{
     private static final String API_KEY = "e087180b714c4af48d7b30dbb9200f06";
     private static final String AUDIO_PATH = String.valueOf(Main.class.getResource("media/audio.wav"));
 
@@ -25,35 +26,11 @@ public class Speaker {
 
     private static volatile boolean haveInternet = true;
 
-    public static void speakWord(String word) throws Exception {
-        Runnable runnable = () -> {
-            try {
-                haveInternet = true;
-                VoiceProvider tts = new VoiceProvider(API_KEY);
+    private static String word;
 
-                VoiceParameters params = new VoiceParameters(word, Languages.English_UnitedStates);
-                params.setCodec(AudioCodec.WAV);
-                params.setFormat(AudioFormat.Format_44KHZ.AF_44khz_16bit_stereo);
-                params.setLanguage(language);
-                params.setVoice(Name);
-                params.setBase64(false);
-                params.setSSML(false);
-                params.setRate(0);
-
-                byte[] voice = tts.speech(params);
-
-                // Play the voice
-                playAudio(voice);
-
-                FileOutputStream fos = new FileOutputStream(AUDIO_PATH);
-                fos.write(voice, 0, voice.length);
-                fos.flush();
-                fos.close();
-            } catch (Exception e) {
-                 haveInternet = false;
-            }
-        };
-        Thread thread = new Thread(runnable);
+    public static void speakWord(String w) throws Exception {
+        word = w;
+        Thread thread = new Speaker();
         thread.start() ;
         Thread.currentThread().sleep(50);
         if(haveInternet == false){
@@ -71,4 +48,32 @@ public class Speaker {
         audioInputStream.close();
     }
 
+    @Override
+    public void run(){
+        try {
+            haveInternet = true;
+            VoiceProvider tts = new VoiceProvider(API_KEY);
+
+            VoiceParameters params = new VoiceParameters(word, Languages.English_UnitedStates);
+            params.setCodec(AudioCodec.WAV);
+            params.setFormat(AudioFormat.Format_44KHZ.AF_44khz_16bit_stereo);
+            params.setLanguage(language);
+            params.setVoice(Name);
+            params.setBase64(false);
+            params.setSSML(false);
+            params.setRate(0);
+
+            byte[] voice = tts.speech(params);
+
+            // Play the voice
+            playAudio(voice);
+
+            FileOutputStream fos = new FileOutputStream(AUDIO_PATH);
+            fos.write(voice, 0, voice.length);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            haveInternet = false;
+        }
+    }
 }
